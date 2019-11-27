57dcaad103cc17dd2b5f8bc2e025d3413f08037f
Andres Ochoa <andres_ochoa91@hotmail.com>
2019-11-26 18:59:55 -0800

    Updates

---
+++ precache-manifest.bc8ce044163dfc98a0cbf1f15938932d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1734a6445cea107c68b0ac8aab2c7ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7896b3d240e78f2b5c30",
+    "url": "/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "ea63319f90d4f88cb5a3",
+    "url": "/static/css/main.c568845b.chunk.css"
+  },
+  {
+    "revision": "7896b3d240e78f2b5c30",
+    "url": "/static/js/2.690a5154.chunk.js"
+  },
+  {
+    "revision": "ea63319f90d4f88cb5a3",
+    "url": "/static/js/main.5f89085a.chunk.js"
+  },
+  {
+    "revision": "d031869627c0a9e3fd74",
+    "url": "/static/js/runtime-main.c4eea047.js"
+  }
+]);
