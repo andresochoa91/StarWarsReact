d649fb54036b1acec96cd1e66b58e4b65a9e979e
Andres Ochoa <andres_ochoa91@hotmail.com>
2019-11-28 22:37:57 -0800

    Updates

---
+++ precache-manifest.46f67fa1f579a1e693dd43e19d3a8580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404b33b3651586d12ab631ba1cc01703",
+    "url": "/StarWarsReactFinal/index.html"
+  },
+  {
+    "revision": "7896b3d240e78f2b5c30",
+    "url": "/StarWarsReactFinal/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "ec3a1cdfae39daab2687",
+    "url": "/StarWarsReactFinal/static/css/main.c568845b.chunk.css"
+  },
+  {
+    "revision": "7896b3d240e78f2b5c30",
+    "url": "/StarWarsReactFinal/static/js/2.690a5154.chunk.js"
+  },
+  {
+    "revision": "ec3a1cdfae39daab2687",
+    "url": "/StarWarsReactFinal/static/js/main.7e151106.chunk.js"
+  },
+  {
+    "revision": "c8421da6d202f515273e",
+    "url": "/StarWarsReactFinal/static/js/runtime-main.7f756b21.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc8ce044163dfc98a0cbf1f15938932d.js"
+  "/StarWarsReactFinal/precache-manifest.46f67fa1f579a1e693dd43e19d3a8580.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/StarWarsReactFinal/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
